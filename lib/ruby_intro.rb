4358c838cb025fab22ebfb0c39881f1b66a020b3
tito Domingos <muandatito@gmail.com>
2018-07-12 16:04:09 +0200

    hello function green

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,23 +7,34 @@ def sum arr
 end
 
 def max_2_sum arr
-  return 0 if arr.empty?
-  return arr[0] if arr.length == 1
-  arr.sort!
-  sum [arr[-1], arr[-2]]
-
-
-  # YOUR CODE HERE
+  sum arr.sort.reverse.take(2)
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  i = 0
+
+  while i < arr.length
+    current = arr[i]
+    j=0
+    while  j< arr.length
+      unless j == i
+        sum = current + arr[j]
+        return true if sum == n
+      end
+      j += 1
+    end
+    i +=1
+  end
+
+  false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
